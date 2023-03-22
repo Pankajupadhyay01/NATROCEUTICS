d2d9d38e39a358a83323dfaae07e4952dfe9062a
pankajupadhyay01 <pankajup.git01@gmail.com>
2023-03-22 23:14:40 +0530

    nav bar created

--- src/component.jsx/Hero.jsx
+++ src/component.jsx/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <div className='flex flex-col h-[65vh] bg-red-400 justify-center w-[90%] m-auto '>
+
+                <div className='text-[82px]'>
+                    <div className='text-center'>Enhancing your experience of life </div>
+                    <div>through nature and science.</div>
+                </div>
+                <div>
+                    <div>We are here to deliver you the best available nature-based therapeutics the world’s leading experts have to offer.</div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
